a8dab2448e3b9e76a4077d7c4d673180b5ecb4f1
yanalexander <yan.venera@hotmail.com>
2019-03-23 13:11:56 -0300

    Second commit

---
+++ src/main/java/br/com/cassandaconverter/CassandraConverterApplication.java
@@ -0,0 +1,13 @@
+package br.com.cassandaconverter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CassandraConverterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CassandraConverterApplication.class, args);
+	}
+
+}
